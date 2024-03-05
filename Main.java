49691d6e6e63f98f2535031693e573602469ca98
Crys <cristian.toanca@emanuel.ro>
2024-03-05 10:07:19 +0200

    Proj1

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void Main (String[] args) {
+
+        Sertar sertar = new Sertar(1,2,3);
+        sertar.tiparesteSertar();
+
+        Sertar s1 = new Sertar(5,3,2);
+        Sertar s2 = new Sertar(4,2,6);
+        Birou birou = new Birou(10,10,10,s1,s2);
+        birou.tiparesteBirou();
+    }
+}
---
+++ Sertar.java
@@ -0,0 +1,13 @@
+public class Sertar {
+    private int l, L, H;
+
+    public Sertar(int l, int l2, int h) {
+        this.l = l;
+        this.L = l2;
+        this.H = h;
+    }
+    public void tiparesteSertar() {
+        System.out.println("Sertar"+l+" "+L+" "+H);
+    }
+
+}
